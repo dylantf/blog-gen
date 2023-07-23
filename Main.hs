5b98441e4dd4ac260e90197e5f8068b8b417c48d
dylantf <dylantf@gmail.com>
2023-07-22 20:59:17 -0600

    Implement basic modules

---
+++ Main.hs
@@ -0,0 +1,12 @@
+module Main (main) where
+
+import Html
+
+main :: IO ()
+main = putStrLn (render myHtml)
+
+myHtml :: Html
+myHtml =
+  html_
+    "Hello title"
+    (append_ (h1_ "Dylan's Blog") (p_ "Welcome to my blog"))
